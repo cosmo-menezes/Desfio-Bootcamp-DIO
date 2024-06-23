87bb371a4e05a6e7c5ab94a8389f8ae389a2aae3
Cosmo Menezes de Souza <65937470+cosmo-menezes@users.noreply.github.com>
2024-06-23 11:41:52 +0100

    Add files via upload

---
+++ Desafio-Curso-Bootcampo/src/Curso.java
@@ -0,0 +1,118 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
+
+class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
+
+class Bootcamp {
+    private String nome;
+    private String descricao;
+    private Set<Conteudo> conteudos = new HashSet<>();
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+}
+
+abstract class Conteudo {
+    protected String titulo;
+    protected String descricao;
+
+    public abstract double calcularXp();
+}
+
+class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new HashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new HashSet<>();
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+    }
+
+    public void progredir() {
+        if (!conteudosInscritos.isEmpty()) {
+            Conteudo conteudo = conteudosInscritos.iterator().next();
+            conteudosInscritos.remove(conteudo);
+            conteudosConcluidos.add(conteudo);
+        }
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public double calcularTotalXp() {
+        return conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
+}
